e80281b5789df661f85b887acaca0cabc0834aa9
codethi <docencia.thiago@gmail.com>
2022-11-04 15:44:30 -0300

    alter port

--- src/index.js
+++ src/index.js
@@ -54,7 +54,7 @@ app.post("/receitas", (req, res) => {
   res.status(201).send("Receita criada com sucesso!");
 });
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running in port: ${port}`);
